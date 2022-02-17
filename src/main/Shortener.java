73b52f8cd3ae0f7e494b6b28ee16f5eb9c8b1f31
Dawid Bujnicki <89477976+Bujnik@users.noreply.github.com>
2022-02-17 09:38:59 +0100

    init
    Shortener class with stubs for getId and getString methods
    Solution - main method, for testing

---
+++ src/main/Shortener.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Shortener {
+    public Long getId(String string){
+        return new Long(0);
+    }
+
+    public String getString(Long id){
+        return null;
+    }
+}
