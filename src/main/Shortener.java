6924225182f893fa762fa0433984b6f764db7c1c
Dawid Bujnicki <89477976+Bujnik@users.noreply.github.com>
2022-02-17 13:14:03 +0100

    Part3
    Shortener:
    -> Implemented getId
    -> Implemented getString

--- src/main/Shortener.java
+++ src/main/Shortener.java
@@ -1,11 +1,35 @@
 package main;
 
+import main.strategy.StorageStrategy;
+
 public class Shortener {
-    public Long getId(String string){
-        return 0L;
+    //Holds identifier of last string added to the repository
+    private Long lastId = 0L;
+    private StorageStrategy storageStrategy;
+
+    public Shortener(StorageStrategy storageStrategy) {
+        this.storageStrategy = storageStrategy;
     }
 
-    public String getString(Long id){
-        return null;
+    /**
+     * This method will work with threads
+     * If passed string is not in the storage,
+     * it will be added as new entry
+     */
+    public synchronized Long getId(String string){
+        if (storageStrategy.containsValue(string)) return storageStrategy.getKey(string);
+        else {
+            lastId++;
+            storageStrategy.put(lastId, string);
+            return lastId;
+        }
+    }
+
+    /**
+     * This method returns String for given key
+     * It will work with threads
+     */
+    public synchronized String getString(Long id){
+        return storageStrategy.getValue(id);
     }
 }
